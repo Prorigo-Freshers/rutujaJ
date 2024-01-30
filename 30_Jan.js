8d5d9009860742016358cb3050f19043b75ba67d
rutujajaid <73790782+rutujajaid@users.noreply.github.com>
2024-01-30 23:49:15 +0530

    Update 30_Jan.js

--- 30_Jan.js
+++ 30_Jan.js
@@ -1,14 +1,3 @@
-function twoSum(nums,target) {
-for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-        if (nums[i]+nums[j] === target) {
-                return [i,j];
-            }
-        }
-    }
-  return [];
-}
-
 const nums1 = [2,7,11,15];
 const target1 = 9;
 const result1 = twoSum(nums1,target1);
@@ -23,3 +12,15 @@ const nums3 = [3,3];
 const target3 = 6;
 const result3 = twoSum(nums3,target3);
 console.log(result3);  
+
+function twoSum(nums,target) {
+for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i]+nums[j] === target) {
+                return [i,j];
+            }
+        }
+    }
+  return [];
+}
+
