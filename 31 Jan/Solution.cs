6d703bc2bc3dc114533a0618fc95738a43dad260
rutujajaid <73790782+rutujajaid@users.noreply.github.com>
2024-01-31 23:53:21 +0530

    Solution.cs

---
+++ 31 Jan/Solution.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0 &&  x != 0) {
+            return false;
+        }
+        
+        int reversedNum = 0;
+        int originalNum = x;
+        
+        while (x > 0) {
+            int digit = x % 10;
+            reversedNum = reversedNum * 10 + digit;
+            x /= 10;
+        }
+        
+        return originalNum == reversedNum;
+    }
+}
+
+class Program {
+    static void Main() {
+        var solution = new Solution();
+        Console.WriteLine(solution.IsPalindrome(919));   
+        Console.WriteLine(solution.IsPalindrome(-121));  
+        Console.WriteLine(solution.IsPalindrome(10));   
+    }
+}
