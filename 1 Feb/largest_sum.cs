0dccecff3fb994df41a215ce84b3143231bc0c8c
rutujajaid <73790782+rutujajaid@users.noreply.github.com>
2024-02-01 23:22:21 +0530

    largest_sum.cs

---
+++ 1 Feb/largest_sum.cs
@@ -0,0 +1,16 @@
+using System;
+class largest_sum
+{
+   public static void Main(string[] args)
+    {
+        int[] input = { 1, 2, 3, 4, -10 };
+        int currentSum = input[0];
+        int maxSum = input[0];
+
+        for (int i = 1; i < input.Length; i++) {
+            currentSum = Math.Max(input[i],currentSum + input[i]);
+            maxSum = Math.Max(maxSum,currentSum;
+        }
+        Console.WriteLine("Output: " + maxSum);
+    }
+}
