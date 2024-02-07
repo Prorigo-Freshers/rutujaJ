f358e91d330e3c8846b6b888d041749001e23a5b
rutujajaid <73790782+rutujajaid@users.noreply.github.com>
2024-02-07 23:09:18 +0530

    Update leader.cs

--- 1 Feb/leader.cs
+++ 1 Feb/leader.cs
@@ -1,3 +1,7 @@
+﻿// Problem Description : Given an integer array A containing N distinct integers, you have to find all the leaders in array A. An element is a leader if it is strictly greater than all the elements to its right side. NOTE: The rightmost element is always a leader.
+
+
+
 using System;
 class leader
 {
