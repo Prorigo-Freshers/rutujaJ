207cbe3b073f52dd0306a4b64c3b3afb97faf977
rutujajaid <73790782+rutujajaid@users.noreply.github.com>
2024-02-08 19:39:21 +0530

    Create even_odd.cs

---
+++ 8 Feb/even_odd.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+       int num =11;
+       if((num & 1) == 0)
+       {Console.WriteLine("Even");}
+       else
+       {Console.WriteLine("Odd");}
+    }
+}
