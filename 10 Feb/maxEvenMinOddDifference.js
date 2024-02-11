270028983e89ede0b6ba794a89960bb20b4ef07f
rutujajaid <73790782+rutujajaid@users.noreply.github.com>
2024-02-12 00:04:49 +0530

    maxEvenMinOddDifference.js

--- 10 Feb/maxEvenMinOddDifference.js
+++ 10 Feb/maxEvenMinOddDifference.js
@@ -1,7 +1,9 @@
+//Given an array of integers A of size N.
+//Return the difference between the maximum among all even numbers of A and the minimum among all odd numbers in A.
+
 function maxEvenMinOddDifference(A) {
     let maxEven = -Infinity;
-    let minOdd = Infinity;
-    
+    let minOdd = Infinity; 
     for (let i = 0; i < A.length; i++) {
         const num = A[i];
         if (num % 2 === 0 && num > maxEven) {
@@ -9,14 +11,9 @@ function maxEvenMinOddDifference(A) {
         } else if (num % 2 !== 0 && num < minOdd) {
             minOdd = num;
         }
-    }
-    
+    }    
     return maxEven - minOdd;
 }
 
-// Example usage:
-const A1 = [0, 2, 9];
 const A2 = [5, 17, 100, 1];
-
-console.log(maxEvenMinOddDifference(A1)); // Output: -7
-console.log(maxEvenMinOddDifference(A2)); // Output: 99
+console.log(maxEvenMinOddDifference(A2)); 
